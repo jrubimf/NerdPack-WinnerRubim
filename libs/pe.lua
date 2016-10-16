d5090a49bb2e8a809fc4aae8b65d40cb9e190ce5
jrubimf <jrubimf@gmail.com>
2016-10-16 18:28:36 -0200

    Update pe.lua
    meele to melee

--- libs/pe.lua
+++ libs/pe.lua
@@ -1,48 +1,65 @@
---/dump NeP.DSL.Conditions['rpdeficiet']('player')
---/dump NeP.DSL.Conditions['rprint']('Hi')
---/dump NeP.DSL.Conditions['rubimarea']
---/dump NeP.DSL.Conditions['areattd']('player')
---/dump NeP.DSL.Conditions['rubimarea.enemies']('player',8)
-NeP.DSL.RegisterConditon('rpdeficiet', function(target)
+--/dump NeP.DSL:Get['rpdeficiet']('player')
+--/dump NeP.DSL:Get['rprint']('Hi')
+--/dump NeP.DSL:Get['rubimarea']
+--/dump NeP.DSL:Get('areattd')('player')
+--/dump NeP.DSL:Get('area')('player')
+--/dump NeP.DSL:Get('movingfor')('player')
+--/dump NeP.DSL:Get('blood.rotation')('player')
+--isin
+--/dump NeP.DSL:Get('onmelee')('player')
+----actions+=/hamstring,if=buff.battle_cry_deadly_calm.remains>cooldown.hamstring.remains
+
+
+--FUCK YEAH Gabbzz!
+NeP.DSL:Register('rotation', function(rotation)
+	if rotation == NeP.Library:Fetch('Rubim').BloodMaster() then return true end
+	return false
+end)
+
+NeP.DSL:Register('onmelee', function(rotation)
+	local isitokay = NeP.Library:Fetch('Rubim').meleeRange()
+	return isitokay
+end)
+
+NeP.DSL:Register('blood.rotation', function(target, rotation)
+	if rotation == NeP.Library:Fetch('Rubim').BloodMaster() then return true end
+	return false
+end)
+
+NeP.DSL:Register('rpdeficiet', function(target)
 	return (UnitPowerMax(target, SPELL_POWER_RUNIC_POWER)) - (UnitPower(target, SPELL_POWER_RUNIC_POWER))
 end)
 
-NeP.DSL.RegisterConditon('equipped', function(target, item)
-	if IsEquippedItem(item) == true then return true else return false end
-end)
-
-NeP.DSL.RegisterConditon('rprint', function(text)
+NeP.DSL:Register('rprint', function(text)
 	print(text)
 end)
 
-NeP.DSL.RegisterConditon("rubimarea.enemies", function(unit, distance)
+NeP.DSL:Register("rubimarea.enemies", function(unit, distance)
 	local total = 0
 	local distance = tonumber(distance)
 	if UnitExists(unit) then
-		for i=1, #NeP.OM['unitEnemie'] do
-			local Obj = NeP.OM['unitEnemie'][i]
+		for GUID, Obj in pairs(NeP.OM:Get('Enemy')) do
 			if UnitExists(Obj.key) and UnitHealth(Obj.key) > 0 and not UnitIsDeadOrGhost(Obj.key)
-			and (UnitAffectingCombat(Obj.key) or isDummy(Obj.key))
-			and (NeP.Engine.Distance(unit, Obj.key) <= distance) then
+			and (UnitAffectingCombat(Obj.key) or NeP.DSL:Get('isdummy')(Obj.key))
+			and (NeP.Protected.Distance(unit, Obj.key) <= tonumber(distance)) then
 				total = total +1
 			end
 		end
 	end
-	if total == 0 and UnitExists('target') and UnitHealth('target') > 0 and IsSpellInRange(GetSpellInfo(meeleSpell), "target") == 1 then
+	if total == 0 and UnitExists('target') and UnitHealth('target') > 0 and IsSpellInRange(GetSpellInfo(meleeSpell), "target") == 1 then
 		total = 1
 	end
 	return total
 end)
 
-NeP.DSL.RegisterConditon("areattd", function(target)
+NeP.DSL:Register("areattd", function(target)
 	local ttd = 0
 	local total = 0
-	for i=1,#NeP.OM['unitEnemie'] do
-		local Obj = NeP.OM['unitEnemie'][i]	
-		if Obj.distance <= 6 and (UnitAffectingCombat(Obj.key) or Obj.is == 'dummy') then
-			if NeP.DSL.Conditions["deathin"](Obj.key) < 8 then
+	for GUID, Obj in pairs(NeP.OM:Get('Enemy')) do
+		if NeP.Protected.Distance("player", Obj.key) <= tonumber(8) and (UnitAffectingCombat(Obj.key) or NeP.DSL:Get('isdummy')(Obj.key)) then
+			if NeP.DSL:Get("deathin")(Obj.key) < 8 then
 				total = total+1
-				ttd = NeP.DSL.Conditions["deathin"](Obj.key) + ttd
+				ttd = NeP.DSL:Get("deathin")(Obj.key) + ttd
 			end
 		end
 	end
@@ -51,4 +68,24 @@ NeP.DSL.RegisterConditon("areattd", function(target)
 	else
 		return 9999999999
 	end
+end)
+
+NeP.DSL:Register("allstacked", function(target)
+	local arethey = false
+	local allenemies = 0
+	local closeenemies = 0
+	local total = 0
+	for GUID, Obj in pairs(NeP.OM:Get('Enemy')) do
+		if NeP.DSL:Get('combat')(Obj.key)
+		and NeP.Protected.Distance("player", Obj.key) <= tonumber(30) then
+			allenemies = allenemies +1
+		end
+		
+		if NeP.DSL:Get('combat')(Obj.key)
+		and NeP.Protected.Distance("player", Obj.key) <= tonumber(7) then
+			closeenemies = closeenemies+1
+		end
+	end
+	if allenemies > 0 and allenemies == closeenemies then arethey = true end
+	return arethey
 end)
